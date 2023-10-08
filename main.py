dc24af939f50f615b415bc7398848f95c0fe0d87
taquito <taquitodebistek23@gmail.com>
2023-10-08 13:26:41 -0600

    agregar main de destination

--- main.py
+++ main.py
@@ -46,8 +46,17 @@ def organism():
 
 @app.route('/destinations')
 def destinations():
+    destination="tierra"
+    distance="0km"
+    time="0hr"
+    warning="""
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget ligula eu arcu tincidunt molestie. Integer posuere eros sed lorem iaculis placerat. Nunc ullamcorper augue metus, vitae fermentum justo hendrerit nec. Aliquam erat volutpat. Fusce eu justo at neque viverra fermentum ut at sapien.
+    """
+    resources="""
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget ligula eu arcu tincidunt molestie. Integer posuere eros sed lorem iaculis placerat. Nunc ullamcorper augue metus, vitae fermentum justo hendrerit nec. Aliquam erat volutpat. Fusce eu justo at neque viverra fermentum ut at sapien.
+    """
     template, context = destinations_view(app)
-    return render_template(template, **context)
+    return render_template('destinations.html',destination=destination,distance=distance,time=time,warning=warning,resources=resources)
 
 
 @app.route('/itinerary')
