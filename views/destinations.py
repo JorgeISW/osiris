f524f0b60b5c263d7d4a1223ff03f690c39e9a1e
Jorge Salazar <jor.idw.mx@gmail.com>
2023-10-10 21:18:23 -0600

    Automatical image added to Destimations page

--- views/destinations.py
+++ views/destinations.py
@@ -6,7 +6,7 @@ def destinations_view(app, planet_id):
     context = {}
 
     with bd_connector.connect(app).cursor() as cur:
-        cur.execute(f"SELECT * FROM infotravel where ID = {planet_id}")
+        cur.execute(f"SELECT infotravel.*, planetaryinfo.img_name FROM infotravel INNER JOIN planetaryinfo ON infotravel.ID = planetaryinfo.ID where infotravel.ID = {planet_id}")
         planeta = cur.fetchall()
         context['planeta'] = planeta[0] if planeta else 0
 
